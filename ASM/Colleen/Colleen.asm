010f73ed149f535ed53bd6d1ae7ec98960c54583
William <73087063+lwwilliam@users.noreply.github.com>
2024-09-20 09:56:03 +0000

    colleen asm

--- ASM/Colleen/Colleen.asm
+++ ASM/Colleen/Colleen.asm
@@ -1,14 +1,20 @@
 section .data
-msg db 'Hello, world!', 0xa;
+msg db 'Hello',13,10,'world!',13,10,'$', 0xa
 len equ $ - msg
 
 section .text
 global _start
 
+; comment
 _start:
-    ; calculate the length of string
-    mov rdi, msg
-    mov rdx, len
+    ; comment in main
     mov rax, 1
+    mov rdi, 1          ; file descriptor 1 is stdout
+    mov rsi, msg        ; pointer to the message
+    mov rdx, len        ; length of the message
+    syscall
 
+    ; exit the program
+    mov rax, 60         ; syscall number for sys_exit
+    xor rdi, rdi        ; exit code 0
     syscall
