d6bb596e0d92a019669dac30f677385fd2d4533b
William <73087063+lwwilliam@users.noreply.github.com>
2024-09-23 02:47:55 +0000

    asm

--- ASM/Colleen/Colleen.asm
+++ ASM/Colleen/Colleen.asm
@@ -1,16 +1,16 @@
 section .data
-msg db 'Hello',13,10,'world!',13,10,'$', 0xa
+msg db 'section .data',10,'msg db',10,'len equ $ - msg',10,10, 'section .text',10,'global main',10,10,59,' comment',10,'main:',10,'    ',59,' comment in main',10,'    mov rax, 1',10,'    mov rdi, 1',10,'    mov rsi, msg',10,'    mov rdx, len',10,'    syscall',10,10,'    ',59,' exit the program',10,'    mov rax, 60',10,'    xor rdi, rdi',10,'    syscall',0xa
 len equ $ - msg
 
 section .text
-global _start
+global main
 
 ; comment
-_start:
+main:
     ; comment in main
     mov rax, 1
     mov rdi, 1          ; file descriptor 1 is stdout
-    mov rsi, msg        ; pointer to the message
+    lea rsi, msg        ; pointer to the message
     mov rdx, len        ; length of the message
     syscall
 
