3462afafd573d12ece2134a5bce246d882de8199
William <73087063+lwwilliam@users.noreply.github.com>
2024-09-27 01:38:04 +0000

    3 define in grace asm

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -1,8 +1,10 @@
 %define GLOBAL global main
+%define MAIN main
+%define STR "%4$cdefine GLOBAL global main%3$c%4$cdefine MAIN main%3$c%4$cdefine STR %2$c%s%2$c%3$c%3$c%4$cmacro macro 0%3$csection .data%3$cmsg db STR,0%3$cfileName db %2$cGrace_kid.s%2$c,0%3$c%3$csection .text%3$cGLOBAL%3$cextern dprintf%3$c%3$c; comment%3$cMAIN:%3$c    mov rax, 2%3$c    mov rdi, fileName%3$c    mov rsi, 0102o%3$c    mov rdx, 00600o%3$c    syscall%3$c%3$c    push rbp%3$c    mov rbp, rsp%3$c    mov rdi, rax%3$c    lea rsi, [msg]%3$c    lea rdx, [msg]%3$c    mov rcx, 34%3$c    mov r8, 10%3$c    mov r9, 37%3$c    call dprintf%3$c    mov rsp, rbp%3$c    pop rbp%3$c    ret%3$c%4$cendmacro%3$c%3$cmacro"
 
 %macro macro 0
 section .data
-msg db "%4$cdefine GLOBAL global main%3$c%3$c%4$cmacro macro 0%3$csection .data%3$cmsg db %2$c%s%2$c,0%3$cfileName db %2$cGrace_kid.s%2$c,0%3$c%3$csection .text%3$cGLOBAL%3$cextern dprintf%3$c%3$c; comment%3$cmain:%3$c    mov rax, 2%3$c    mov rdi, fileName%3$c    mov rsi, 0102o%3$c    mov rdx, 00600o%3$c    syscall%3$c%3$c    push rbp%3$c    mov rbp, rsp%3$c    mov rdi, rax%3$c    lea rsi, [msg]%3$c    lea rdx, [msg]%3$c    mov rcx, 34%3$c    mov r8, 10%3$c    mov r9, 37%3$c    call dprintf%3$c    mov rsp, rbp%3$c    pop rbp%3$c    ret%3$c%4$cendmacro%3$c%3$cmacro",0
+msg db STR,0
 fileName db "Grace_kid.s",0
 
 section .text
@@ -10,7 +12,7 @@ GLOBAL
 extern dprintf
 
 ; comment
-main:
+MAIN:
     mov rax, 2
     mov rdi, fileName
     mov rsi, 0102o
