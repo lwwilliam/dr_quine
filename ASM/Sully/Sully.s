d0d451c9faabf7bdcb0bd3d66d688eae2dec0669
William <73087063+lwwilliam@users.noreply.github.com>
2024-12-13 17:31:01 +0000

    sully moment

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,5 +1,5 @@
 section .data
-msg db "section .data%3$cmsg db %2$c%s%2$c,0%3$cfileName db %2$cSully_%4$cd.s%2$c, 0%3$ccompile db %2$cnasm -f elf64 Sully_%4$c1$d.s -o Sully_%4$c1$d.o%2$c, 0%3$cgcc db %2$cgcc -Wall -Wextra -Werror -no-pie Sully_%4$c1$d.o -o Sully_%4$c1$d%2$c, 0%3$cexe db %2$c./Sully_%4$cd%2$c, 0%3$cx db %5$d%3$c%3$csection .bss%3$cformattedFileName resb 20%3$cformattedCompile resb 50%3$cformattedGcc resb 50%3$cformattedExe resb 50%3$c%3$csection .text%3$cglobal main%3$cextern sprintf, dprintf, system%3$c%3$c%4$cmacro create 2%3$c    push rbp%3$c    mov rbp, rsp%3$c    lea rdi, [%4$c1]%3$c    lea rsi, [%4$c2]%3$c    movzx rdx, byte [x]%3$c    call sprintf%3$c    mov rsp, rbp%3$c    pop rbp%3$c%3$c    push rbp%3$c    mov rbp, rsp%3$c    lea rdi, [%4$c1]%3$c    call system%3$c    mov rsp, rbp%3$c    pop rbp%3$c%4$cendmacro%3$c%3$cexecve:%3$c    create formattedCompile, compile%3$c    create formattedGcc, gcc%3$c    create formattedExe, exe%3$c    ret%3$c%3$cmain:%3$c    push rbp%3$c    mov rbp, rsp%3$c    dec byte [x]%3$c    lea rdi, [formattedFileName]%3$c    lea rsi, [fileName]%3$c    movzx rdx, byte [x]%3$c    call sprintf%3$c    mov rsp, rbp%3$c    pop rbp%3$c%3$c    mov rax, 2%3$c    mov rdi, formattedFileName%3$c    mov rsi, 0102o%3$c    mov rdx, 00600o%3$c    syscall%3$c%3$c    push rbp%3$c    mov rbp, rsp%3$c    mov rdi, rax%3$c    lea rsi, [msg]%3$c    lea rdx, [msg]%3$c    mov rcx, 34%3$c    mov r8, 10%3$c    mov r9, 37%3$c    sub rsp, 8%3$c    movzx rbx, byte [x]%3$c    push rbx%3$c    call dprintf%3$c    add rsp, 8%3$c    mov rsp, rbp%3$c    pop rbp%3$c%3$c    cmp byte [x], 0%3$c    jg execve%3$c    ret",0
+msg db "section .data%3$cmsg db %2$c%s%2$c,0%3$cfileName db %2$cSully_%4$cd.s%2$c, 0%3$ccompile db %2$cnasm -f elf64 Sully_%4$c1$d.s -o Sully_%4$c1$d.o%2$c, 0%3$cgcc db %2$cgcc -Wall -Wextra -Werror -no-pie Sully_%4$c1$d.o -o Sully_%4$c1$d%2$c, 0%3$cexe db %2$c./Sully_%4$cd%2$c, 0%3$cx db %5$d%3$c%3$csection .bss%3$cformattedFileName resb 20%3$cformattedCompile resb 50%3$cformattedGcc resb 50%3$cformattedExe resb 50%3$c%3$csection .text%3$cglobal main%3$cextern sprintf, dprintf, system%3$c%3$c%4$cmacro create 2%3$c    push rbp%3$c    mov rbp, rsp%3$c    lea rdi, [%4$c1]%3$c    lea rsi, [%4$c2]%3$c    movzx rdx, byte [x]%3$c    call sprintf%3$c    mov rsp, rbp%3$c    pop rbp%3$c%3$c    push rbp%3$c    mov rbp, rsp%3$c    lea rdi, [%4$c1]%3$c    call system%3$c    mov rsp, rbp%3$c    pop rbp%3$c%4$cendmacro%3$c%3$cexecve:%3$c    create formattedCompile, compile%3$c    create formattedGcc, gcc%3$c    create formattedExe, exe%3$c    ret%3$c%3$cend:%3$c    ret%3$c%3$cmain:%3$c    cmp byte [x], 0%3$c    jl end%3$c    push rbp%3$c    mov rbp, rsp%3$c    lea rdi, [formattedFileName]%3$c    lea rsi, [fileName]%3$c    movzx rdx, byte [x]%3$c    call sprintf%3$c    mov rsp, rbp%3$c    pop rbp%3$c%3$c    mov rax, 2%3$c    mov rdi, formattedFileName%3$c    mov rsi, 0102o%3$c    mov rdx, 00600o%3$c    syscall%3$c%3$c    push rbp%3$c    mov rbp, rsp%3$c    mov rdi, rax%3$c    dec byte [x]%3$c    lea rsi, [msg]%3$c    lea rdx, [msg]%3$c    mov rcx, 34%3$c    mov r8, 10%3$c    mov r9, 37%3$c    sub rsp, 8%3$c    movzx rbx, byte [x]%3$c    push rbx%3$c    call dprintf%3$c    add rsp, 8%3$c    mov rsp, rbp%3$c    pop rbp%3$c%3$c    inc byte [x]%3$c    cmp byte [x], 0%3$c    jge execve%3$c    ret",0
 fileName db "Sully_%d.s", 0
 compile db "nasm -f elf64 Sully_%1$d.s -o Sully_%1$d.o", 0
 gcc db "gcc -Wall -Wextra -Werror -no-pie Sully_%1$d.o -o Sully_%1$d", 0
@@ -40,10 +40,14 @@ execve:
     create formattedExe, exe
     ret
 
+end:
+    ret
+
 main:
+    cmp byte [x], 0
+    jl end
     push rbp
     mov rbp, rsp
-    dec byte [x]
     lea rdi, [formattedFileName]
     lea rsi, [fileName]
     movzx rdx, byte [x]
@@ -60,6 +64,7 @@ main:
     push rbp
     mov rbp, rsp
     mov rdi, rax
+    dec byte [x]
     lea rsi, [msg]
     lea rdx, [msg]
     mov rcx, 34
@@ -73,6 +78,7 @@ main:
     mov rsp, rbp
     pop rbp
 
+    inc byte [x]
     cmp byte [x], 0
-    jg execve
+    jge execve
     ret
--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -2,10 +2,10 @@
 #include <strings.h>
 #include <stdlib.h>
 
-#define STR "#include <stdio.h>%c#include <strings.h>%c#include <stdlib.h>%c%c#define STR %c%s%c%c#define FT(x) int main(){%c%cint x = %d;%c%cif (x > 0){char filename[20];char exe[20];sprintf(filename, %cSully_%%d.c%c, x-1);sprintf(exe, %cSully_%%d%c, x-1);FILE * f = fopen(filename, %cw%c);fprintf(f,STR,10,10,10,10,34,STR,34,10,92,10,x-1,92,10,34,34,34,34,34,34,34,34,34,34,10,10,10,10,10);fclose(f);char tmp[100];sprintf(tmp, %cgcc -Wall -Wextra -Werror %%s -o %%s%c,filename,exe);system(tmp);bzero(tmp,100);sprintf(tmp, %c./%%s%c,exe);system(tmp);}}%c%c/*%c    comments%c*/%cFT(x)"
+#define STR "#include <stdio.h>%c#include <strings.h>%c#include <stdlib.h>%c%c#define STR %c%s%c%c#define FT(x) int main(){%c%cint x = %d;%c%cif (x >= 0){char filename[20];char exe[20];sprintf(filename, %cSully_%%d.c%c, x);sprintf(exe, %cSully_%%d%c, x);FILE * f = fopen(filename, %cw%c);fprintf(f,STR,10,10,10,10,34,STR,34,10,92,10,x-1,92,10,34,34,34,34,34,34,34,34,34,34,10,10,10,10,10);fclose(f);char tmp[100];sprintf(tmp, %cgcc -Wall -Wextra -Werror %%s -o %%s%c,filename,exe);system(tmp);bzero(tmp,100);sprintf(tmp, %c./%%s%c,exe);system(tmp);}}%c%c/*%c    comments%c*/%cFT(x)"
 #define FT(x) int main(){\
 int x = 5;\
-if (x > 0){char filename[20];char exe[20];sprintf(filename, "Sully_%d.c", x-1);sprintf(exe, "Sully_%d", x-1);FILE * f = fopen(filename, "w");fprintf(f,STR,10,10,10,10,34,STR,34,10,92,10,x-1,92,10,34,34,34,34,34,34,34,34,34,34,10,10,10,10,10);fclose(f);char tmp[100];sprintf(tmp, "gcc -Wall -Wextra -Werror %s -o %s",filename,exe);system(tmp);bzero(tmp,100);sprintf(tmp, "./%s",exe);system(tmp);}}
+if (x >= 0){char filename[20];char exe[20];sprintf(filename, "Sully_%d.c", x);sprintf(exe, "Sully_%d", x);FILE * f = fopen(filename, "w");fprintf(f,STR,10,10,10,10,34,STR,34,10,92,10,x-1,92,10,34,34,34,34,34,34,34,34,34,34,10,10,10,10,10);fclose(f);char tmp[100];sprintf(tmp, "gcc -Wall -Wextra -Werror %s -o %s",filename,exe);system(tmp);bzero(tmp,100);sprintf(tmp, "./%s",exe);system(tmp);}}
 
 /*
     comments
